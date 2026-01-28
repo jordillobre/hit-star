bde0d74e11a5c1026443d33c004502bfb8dbaf8d
uji-game <jordillobre@gmail.com>
2026-01-28 13:03:06 +0100

    ajustes menu principal 01

--- Assets/scripts/MainMenuController.cs
+++ Assets/scripts/MainMenuController.cs
@@ -6,14 +6,28 @@ using TMPro;
 
 public class MainMenuController : MonoBehaviour
 {
+    [Header("Apartados canvas")]
+    public GameObject canvaInicio;
+    public GameObject canvaJugadores;
+    public GameObject canvaNombres;
+    public GameObject canvaCategorias;
+    public GameObject canvaOpciones;
+    public GameObject canvaAyuda;
 
     public Text numJugadores;
+
+
     private int jugadores;
     private string njugadores;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        if(canvaInicio != null) canvaInicio.SetActive(true);
+        if(canvaJugadores != null) canvaJugadores.SetActive(false);
+        if(canvaNombres != null) canvaNombres.SetActive(false);
+        if(canvaCategorias != null) canvaCategorias.SetActive(false);
+        if(canvaOpciones != null) canvaOpciones.SetActive(false);
+        if(canvaAyuda != null) canvaAyuda.SetActive(false);
     }
 
     // Update is called once per frame
@@ -41,5 +55,32 @@ public class MainMenuController : MonoBehaviour
         {
             Debug.LogWarning("El valor introducido no es un número válido.");
         }
+    }
+
+    public void selJugadores(){
+        if(canvaInicio != null) canvaInicio.SetActive(false);
+        if(canvaJugadores != null) canvaJugadores.SetActive(true);
+    }
+
+    public void verOpciones(){
+        if(canvaInicio != null) canvaInicio.SetActive(false);
+        if(canvaOpciones != null) canvaOpciones.SetActive(true);
+    }
+
+    public void cerrarOpciones(){
+        if(canvaInicio != null) canvaInicio.SetActive(true);
+        if(canvaOpciones != null) canvaOpciones.SetActive(false);
+    }
+    public void verAyuda(){
+        if(canvaInicio != null) canvaInicio.SetActive(false);
+        if(canvaAyuda != null) canvaAyuda.SetActive(true);
+    }
+
+    public void cerrarAyuda(){
+        if(canvaInicio != null) canvaInicio.SetActive(true);
+        if(canvaAyuda != null) canvaAyuda.SetActive(false);
+    }
+    public void cerrarJuego(){
+        
     }
 }
